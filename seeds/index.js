4ec4d0a818ca5f25750fe0b705fd2625429d237c
Wendy Zhang <wendyyuxuanzhang@gmail.com>
2022-05-07 17:23:15 -0400

    seed mongo atlas database

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,8 @@ const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+// mongoose.connect('mongodb://localhost:27017/yelp-camp');
+mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -19,7 +20,8 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '626b5cf0f2b86b395901ec39',
+            // author: '626b5cf0f2b86b395901ec39',
+            author: 'first_user',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque eligendi expedita voluptatum, omnis ducimus obcaecati ut, magnam cum beatae dolore, quaerat praesentium eum? Reiciendis excepturi, aliquid soluta modi repellendus voluptatum.",
